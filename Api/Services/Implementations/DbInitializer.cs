b3df23e1dd1541f67ca7e6154ad085da5de18cef
lemoniDev <support@lemoniafrica.com>
2023-08-23 19:12:16 +0100

    segemented to private functions

--- Api/Services/Implementations/DbInitializer.cs
+++ Api/Services/Implementations/DbInitializer.cs
@@ -21,118 +21,136 @@ namespace Api.Services.Implementations
         {
             try
             {
-                if (_context.Database.GetPendingMigrations().Any())
-                {
-                    _context.Database.Migrate();
-                }
+                Migrate();
+                SeedData();
             }
             catch (Exception e)
             {
+
                 throw;
             }
-            try
+        }
+
+
+        private void SeedData()
+        {
+            DependentData();
+            SystemConfigData();
+            EmployeeData();
+        }
+
+        private void Migrate()
+        {
+            if (_context.Database.GetPendingMigrations().Any())
             {
-                if (!_context.Employees.Any())
-                {
-                    _context.Employees.AddRangeAsync(
-                        new Employee
-                        {
-                            Id = 1,
-                            FirstName = "LeBron",
-                            LastName = "James",
-                            Salary = 75420.99m,
-                            DateOfBirth = new DateTime(1984, 12, 30)
-                        },
-                        new Employee
-                        {
-                            Id = 2,
-                            FirstName = "Ja",
-                            LastName = "Morant",
-                            Salary = 92365.22m,
-                            DateOfBirth = new DateTime(1999, 8, 10)
-                        },
-                        new Employee
-                        {
-                            Id = 3,
-                            FirstName = "Michael",
-                            LastName = "Jordan",
-                            Salary = 143211.12m,
-                            DateOfBirth = new DateTime(1963, 2, 17)
-                        }
-                     ).GetAwaiter().GetResult();
-                    _context.SaveChangesAsync().GetAwaiter().GetResult();
-                }
+                _context.Database.Migrate();
+            }
+        }
 
-                if (!_context.Dependents.Any())
-                {
-                    _context.Dependents.AddRangeAsync(
-                        new Dependent
-                        {
-                            Id = 1,
-                            FirstName = "Spouse",
-                            LastName = "Morant",
-                            Relationship = Relationship.Spouse,
-                            DateOfBirth = new DateTime(1998, 3, 3),
-                            EmployeeId = 2 // Associate with Employee ID 2 (Ja Morant)
-                        },
-                        new Dependent
-                        {
-                            Id = 2,
-                            FirstName = "Child1",
-                            LastName = "Morant",
-                            Relationship = Relationship.Child,
-                            DateOfBirth = new DateTime(2020, 6, 23),
-                            EmployeeId = 2 // Associate with Employee ID 2 (Ja Morant)
-                        },
-                        new Dependent
-                        {
-                            Id = 3,
-                            FirstName = "Child2",
-                            LastName = "Morant",
-                            Relationship = Relationship.Child,
-                            DateOfBirth = new DateTime(2021, 5, 18),
-                            EmployeeId = 2
-                        },
-                        new Dependent
-                        {
-                            Id = 4,
-                            FirstName = "DP",
-                            LastName = "Jordan",
-                            Relationship = Relationship.DomesticPartner,
-                            DateOfBirth = new DateTime(1974, 1, 2),
-                            EmployeeId = 3
-                        }
-                     ).GetAwaiter().GetResult();
-                    _context.SaveChangesAsync().GetAwaiter().GetResult();
 
-                }
-
-                if (!_context.SystemConfig.Any())
-                {
-                    _context.SystemConfig.AddAsync(new SystemConfig
+        private void DependentData()
+        {
+            if (!_context.Dependents.Any())
+            {
+                _context.Dependents.AddRangeAsync(
+                    new Dependent
                     {
-                        BaseCost = 1000,
-                        DependentCost = 600,
-                        AdditionalSalaryThreshold = 80000,
-                        SalaryDeductionRate = 0.02m,
-                        AgeBasedDeduction = 200,
-                        PaycheckPerYear = 26
-                    }).GetAwaiter().GetResult();
-                    _context.SaveChangesAsync().GetAwaiter().GetResult();
-                }
+                        Id = 1,
+                        FirstName = "Spouse",
+                        LastName = "Morant",
+                        Relationship = Relationship.Spouse,
+                        DateOfBirth = new DateTime(1998, 3, 3),
+                        EmployeeId = 2 // Associate with Employee ID 2 (Ja Morant)
+                    },
+                    new Dependent
+                    {
+                        Id = 2,
+                        FirstName = "Child1",
+                        LastName = "Morant",
+                        Relationship = Relationship.Child,
+                        DateOfBirth = new DateTime(2020, 6, 23),
+                        EmployeeId = 2 // Associate with Employee ID 2 (Ja Morant)
+                    },
+                    new Dependent
+                    {
+                        Id = 3,
+                        FirstName = "Child2",
+                        LastName = "Morant",
+                        Relationship = Relationship.Child,
+                        DateOfBirth = new DateTime(2021, 5, 18),
+                        EmployeeId = 2
+                    },
+                    new Dependent
+                    {
+                        Id = 4,
+                        FirstName = "DP",
+                        LastName = "Jordan",
+                        Relationship = Relationship.DomesticPartner,
+                        DateOfBirth = new DateTime(1974, 1, 2),
+                        EmployeeId = 3
+                    }
+                 ).GetAwaiter().GetResult();
+                _context.SaveChangesAsync().GetAwaiter().GetResult();
 
-                var sysConfig = _context.SystemConfig.FirstOrDefaultAsync().GetAwaiter().GetResult();
-                if (sysConfig != null)
-                {
-                    _cacheService.Set("SystemConfiguration", sysConfig);
-                }
             }
-            catch (Exception e)
+
+        }
+
+        private void SystemConfigData()
+        {
+            if (!_context.SystemConfig.Any())
             {
-                throw;
+                _context.SystemConfig.AddAsync(new SystemConfig
+                {
+                    BaseCost = 1000,
+                    DependentCost = 600,
+                    AdditionalSalaryThreshold = 80000,
+                    SalaryDeductionRate = 0.02m,
+                    AgeBasedDeduction = 200,
+                    PaycheckPerYear = 26
+                }).GetAwaiter().GetResult();
+                _context.SaveChangesAsync().GetAwaiter().GetResult();
             }
 
+            var sysConfig = _context.SystemConfig.FirstOrDefaultAsync().GetAwaiter().GetResult();
+            if (sysConfig != null)
+            {
+                _cacheService.Set("SystemConfiguration", sysConfig);
+            }
+        }
 
+        private void EmployeeData()
+        {
+            if (!_context.Employees.Any())
+            {
+                _context.Employees.AddRangeAsync(
+                    new Employee
+                    {
+                        Id = 1,
+                        FirstName = "LeBron",
+                        LastName = "James",
+                        Salary = 75420.99m,
+                        DateOfBirth = new DateTime(1984, 12, 30)
+                    },
+                    new Employee
+                    {
+                        Id = 2,
+                        FirstName = "Ja",
+                        LastName = "Morant",
+                        Salary = 92365.22m,
+                        DateOfBirth = new DateTime(1999, 8, 10)
+                    },
+                    new Employee
+                    {
+                        Id = 3,
+                        FirstName = "Michael",
+                        LastName = "Jordan",
+                        Salary = 143211.12m,
+                        DateOfBirth = new DateTime(1963, 2, 17)
+                    }
+                 ).GetAwaiter().GetResult();
+                _context.SaveChangesAsync().GetAwaiter().GetResult();
+            }
 
         }
     }
